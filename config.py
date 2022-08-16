8e6a6affe56d30c0616c7a7a5bd39518183918c9
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-08-16 15:56:24 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+from requests import get
+
+
+load_dotenv()
+
+API_ID = os.getenv("API_ID", "").strip()
+API_HASH = os.getenv("API_HASH", "").strip()
+BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
