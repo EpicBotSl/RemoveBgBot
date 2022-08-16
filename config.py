c90d5d5d5974847f3ccf0bcf55bb012cd5ae2f35
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-08-16 19:17:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,6 +10,8 @@ API_HASH = os.getenv("API_HASH", "").strip()
 BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
 UNSCREEN_API = os.getenv("UNSCREEN_API", "JuASCiT65NFrcdTcBJbF2z6x").strip()
 REMOVEBG_API = os.getenv("REMOVEBG_API", "8jgjzSPMVqWqKg45KvriAAv7").strip()
+MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://epic:epic@cluster0.257eaxu.mongodb.net/?retryWrites=true&w=majority").strip()
+
 
 #UNSCREEN_API = JuASCiT65NFrcdTcBJbF2z6x
 #REMOVEBG_API = 8jgjzSPMVqWqKg45KvriAAv7
