9e2307b8c669ccede5b87537a526bc78fdb233e4
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-08-16 16:39:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,3 +8,8 @@ load_dotenv()
 API_ID = os.getenv("API_ID", "").strip()
 API_HASH = os.getenv("API_HASH", "").strip()
 BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
+UNSCREEN_API = os.getenv("UNSCREEN_API", "JuASCiT65NFrcdTcBJbF2z6x").strip()
+REMOVEBG_API = os.getenv("REMOVEBG_API", "8jgjzSPMVqWqKg45KvriAAv7").strip()
+
+#UNSCREEN_API = JuASCiT65NFrcdTcBJbF2z6x
+#REMOVEBG_API = 8jgjzSPMVqWqKg45KvriAAv7
