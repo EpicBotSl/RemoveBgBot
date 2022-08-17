e8a8e1d5cff719e4465ec9c92a244a5d6295a1bb
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-08-17 19:07:02 +0530

    Update start.py

--- epbot/module/start.py
+++ epbot/module/start.py
@@ -141,8 +141,8 @@ __ɪ ᴄᴀɴ ʀᴇᴍᴏᴠᴇ ʙᴀᴄᴋɢʀᴏᴜɴᴅꜱ..
 ᴡʜɪᴛʜᴏᴜᴛ ʙᴀᴄᴋɢʀᴏᴜɴᴅ ʙᴀʙʏ !""", reply_markup=START_BUTTON)
 
 
-@bot.on_callback_query()  
-async def tgm(bot, update):  
+@epic.on_callback_query()  
+async def tgm(epic, update):  
     if update.data == "add": 
         await update.answer(
              text="♻️Adding Soon.....",
@@ -185,3 +185,70 @@ async def tgm(bot, update):
          )
     elif update.data == "close":
          await update.message.delete()
+
+@epic.on_message(filters.command("stats")) 
+async def startprivatsjwe(epic, message):
+    countb = await db.total_users_count()
+    countb = await db.total_users_count()
+    count = await epic.get_chat_members_count(-1001620454933)
+    counta = await epic.get_chat_members_count(-1001620454933)
+    text=f"""
+ ❍RmBg Bot Users ❍ : **{countb}**
+ """
+    await epic.send_sticker(message.chat.id, random.choice(STAT_STICKER))
+    await epic.send_message(message.chat.id, text=text)
+
+STAT_STICKER = ["CAACAgQAAxkBAAEFHRditZFgRBAPm-9bkFJUQKOjSEgxoQACfwsAAmgpeVF2roP_0GLhzykE",
+                "CAACAgQAAxkBAAEFHRVitZFYQ_EPOF7Y1GenAAHZOfu6xNIAAj4MAAKd3llQRh5-qJlCwa0pBA",
+                "CAACAgQAAxkBAAEFHRNitZFVEBwdq0uFJDOvDRx2IzdoCwAC5wwAAubdSFEk6BkQ4EbQ1ikE",
+                "CAACAgQAAxkBAAEFHRFitZFRwzQPYrVUQkxVP4yxF2Uw3gAC4AkAAu9GYFGTgHavjO_HLikE",
+                "CAACAgQAAxkBAAEFHQ9itZFNixLf7fEZICaK8DF-Li967wACUAwAAmEq4VF8xFsUvkvQXSkE"              
+         ]
+
+
+@epic.on_message(filters.incoming & filters.chat(-1001609244993))
+async def bchanl(epic, update, broadcast_ids={}): 
+    all_users = await db.get_all_users()
+    broadcast_msg= update
+    while True:
+        broadcast_id = ''.join([random.choice(string.ascii_letters) for i in range(3)])
+        if not broadcast_ids.get(broadcast_id):
+            break
+
+    out = await epic.send_message(-1001741009206,f"Ads Broadcast Started! You will be notified with log file when all the users are notified.")
+    start_time = time.time()
+    total_users = await db.total_users_count()
+    done = 0
+    failed = 0
+    success = 0
+    broadcast_ids[broadcast_id] = dict(total = total_users, current = done, failed = failed, success = success)
+        
+    async with aiofiles.open('broadcastlog.txt', 'w') as broadcast_log_file:
+        async for user in all_users:
+            sts, msg = await send_msg(user_id = int(user['id']), message = broadcast_msg)
+            if msg is not None:
+                await broadcast_log_file.write(msg)
+            if sts == 200:
+                success += 1
+            else:
+                failed += 1
+            if sts == 400:
+                await db.delete_user(user['id'])
+            done += 1
+            if broadcast_ids.get(broadcast_id) is None:
+                break
+            else:
+                broadcast_ids[broadcast_id].update(dict(current = done, failed = failed, success = success))
+        
+    if broadcast_ids.get(broadcast_id):
+        broadcast_ids.pop(broadcast_id)
+    
+    completed_in = datetime.timedelta(seconds=int(time.time()-start_time))
+    await asyncio.sleep(3)
+    await out.delete()
+    
+    if failed == 0:
+        await epic.send_message(-1001618730343, f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.")
+    else:
+        await epic.send_document(-1001618730343, 'broadcastlog.txt', caption=f"broadcast completed in `{completed_in}`\n\nTotal users {total_users}.\nTotal done {done}, {success} success and {failed} failed.")
+    os.remove('broadcastlog.txt')
