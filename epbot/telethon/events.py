b4edf570e4afe90950ce64d75201a85aaae80e4e
NAVANJANA <107739539+EpicBotSl@users.noreply.github.com>
2022-08-19 19:38:01 +0530

    Create events.py

---
+++ epbot/telethon/events.py
@@ -0,0 +1,39 @@
+import inspect
+import re
+from pathlib import Path
+
+from telethon import events
+
+from epbot.telethon.telethon import tbot
+
+CMD_LIST = {}
+
+
+def register(**args):
+    pattern = args.get("pattern")
+    r_pattern = r"^[/]"
+
+    if pattern is not None and not pattern.startswith("(?i)"):
+        args["pattern"] = "(?i)" + pattern
+
+    args["pattern"] = pattern.replace("^/", r_pattern, 1)
+    stack = inspect.stack()
+    previous_stack_frame = stack[1]
+    file_test = Path(previous_stack_frame.filename)
+    file_test = file_test.stem.replace(".py", "")
+    reg = re.compile("(.*)")
+
+    if pattern is not None:
+        try:
+            cmd = re.search(reg, pattern)
+            try:
+                cmd = cmd.group(1).replace("$", "").replace("\\", "").replace("^", "")
+            except BaseException:
+                pass
+
+            try:
+                CMD_LIST[file_test].append(cmd)
+            except BaseException:
+                CMD_LIST.update({file_test: [cmd]})
+        except BaseException:
+            pass
